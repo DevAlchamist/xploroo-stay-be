a3adb59740a8ce17cfdfc5e8e4dc9c551ae398b9
DevAlchamist <devalchamist@gmail.com>
2025-03-23 19:43:30 +0530

    Update

--- src/api/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("../app"); // Import your app.js
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-const serverless = require("serverless-http"); // Use serverless-http to adapt Express for serverless
-
-module.exports.handler = serverless(app); // Export it as a serverless function
